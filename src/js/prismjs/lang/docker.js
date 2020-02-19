784b85f5849d473441314af9223631ce44145755
Gary Lo <logap@me.com>
2020-02-20 01:26:45 +0800

    refactor to consistent style

--- src/js/prismjs/lang/docker.js
+++ src/js/prismjs/lang/docker.js
@@ -1,12 +1,13 @@
-Prism.languages.docker = {
-    'keyword': {
-        pattern: /(^\s*)(?:ADD|ARG|CMD|COPY|ENTRYPOINT|ENV|EXPOSE|FROM|HEALTHCHECK|LABEL|MAINTAINER|ONBUILD|RUN|SHELL|STOPSIGNAL|USER|VOLUME|WORKDIR)(?=\s)/mi,
-        lookbehind: true
+;(function(Prism) {
+  Prism.languages.docker = {
+    keyword: {
+      pattern: /(^\s*)(?:ADD|ARG|CMD|COPY|ENTRYPOINT|ENV|EXPOSE|FROM|HEALTHCHECK|LABEL|MAINTAINER|ONBUILD|RUN|SHELL|STOPSIGNAL|USER|VOLUME|WORKDIR)(?=\s)/im,
+      lookbehind: true
     },
-    'string': /("|')(?:(?!\1)[^\\\r\n]|\\(?:\r\n|[\s\S]))*\1/,
-    'comment': /#.*/,
-    'punctuation': /---|\.\.\.|[:[\]{}\-,|>?]/
-};
+    string: /("|')(?:(?!\1)[^\\\r\n]|\\(?:\r\n|[\s\S]))*\1/,
+    comment: /#.*/,
+    punctuation: /---|\.\.\.|[:[\]{}\-,|>?]/
+  }
 
-Prism.languages.dockerfile = Prism.languages.docker;
-
+  Prism.languages.dockerfile = Prism.languages.docker
+})(Prism)
