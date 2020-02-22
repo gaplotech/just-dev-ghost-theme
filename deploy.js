c33154e5010a91c9b46e8cf929f32a957761b984
Gary Lo <logap@me.com>
2020-02-22 16:56:19 +0800

    add deploy script

---
+++ deploy.js
@@ -0,0 +1,41 @@
+// Create a token without the client
+const jwt = require('jsonwebtoken')
+const axios = require('axios')
+const FormData = require('form-data')
+const fs = require('fs')
+const path = require('path')
+
+// Admin API key goes here
+const key = process.env.GHOST_ADMIN_API_KEY
+
+if (!key) {
+  throw new Error('missing GHOST_ADMIN_API_KEY')
+}
+
+// Split the key into ID and SECRET
+const [id, secret] = key.split(':')
+
+// Create the token (including decoding secret)
+const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
+  keyid: id,
+  algorithm: 'HS256',
+  expiresIn: '5m',
+  audience: `/v3/admin/`
+})
+
+// Make an authenticated request to create a post
+const url = 'https://gaplotech.ghost.io/ghost/api/v3/admin/themes/upload/'
+const themePath = path.resolve(__dirname, 'dist/gaplotech-blog-theme.zip')
+
+const formData = new FormData()
+// Append any files to the request
+formData.append('file', fs.createReadStream(themePath), { knownLength: fs.statSync(themePath).size })
+const headers = {
+  ...formData.getHeaders(),
+  'Content-Length': formData.getLengthSync(),
+  Authorization: `Ghost ${token}`
+}
+axios
+  .post(url, formData, { headers })
+  .then(response => console.log(JSON.stringify(response.data)))
+  .catch(error => console.error('failed to upload ghost theme', error.response.data))
