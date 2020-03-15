98691a805c8c564184e40af33f1c1f195e162557
Gary Lo <logap@me.com>
2020-03-15 19:48:26 +0800

    enhance deployment script to work with multiple ghost

--- deploy.js
+++ deploy.js
@@ -4,48 +4,51 @@ const axios = require('axios')
 const FormData = require('form-data')
 const fs = require('fs')
 const path = require('path')
-
-// Load .env config
-require('dotenv').config()
-
 const themeName = require('./package.json').name
-const serverUrl = process.env.GHOST_SERVER
 
-// Admin API key goes here
-const key = process.env.GHOST_ADMIN_API_KEY
+// Load deployment config
+const { deployments } = require('./secret.json') || process.env.secret || {}
 
-if (!serverUrl) {
-  throw new Error('environment variable missing GHOST_SERVER')
+if (!deployments) {
+  throw new Error('deployment secret not found')
 }
 
-if (!key) {
-  throw new Error('environment variable missing GHOST_ADMIN_API_KEY')
+async function deploy({ serverUrl, apiKey }) {
+  if (!serverUrl) {
+    throw new Error('environment variable missing GHOST_SERVER')
+  }
+
+  if (!apiKey) {
+    throw new Error('environment variable missing GHOST_ADMIN_API_KEY')
+  }
+
+  // Split the key into ID and SECRET
+  const [id, secret] = apiKey.split(':')
+
+  // Create the token (including decoding secret)
+  const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
+    keyid: id,
+    algorithm: 'HS256',
+    expiresIn: '5m',
+    audience: `/v3/admin/`
+  })
+
+  // Make an authenticated request to create a post
+  const url = `${serverUrl}/ghost/api/v3/admin/themes/upload/`
+  const themePath = path.resolve(__dirname, `dist/${themeName}.zip`)
+
+  const formData = new FormData()
+  // Append any files to the request
+  formData.append('file', fs.createReadStream(themePath), { knownLength: fs.statSync(themePath).size })
+  const headers = {
+    ...formData.getHeaders(),
+    'Content-Length': formData.getLengthSync(),
+    Authorization: `Ghost ${token}`
+  }
+  await axios
+    .post(url, formData, { headers })
+    .then(response => console.log(JSON.stringify(response.data)))
+    .catch(error => console.error('failed to upload ghost theme', error.response.data))
 }
 
-// Split the key into ID and SECRET
-const [id, secret] = key.split(':')
-
-// Create the token (including decoding secret)
-const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
-  keyid: id,
-  algorithm: 'HS256',
-  expiresIn: '5m',
-  audience: `/v3/admin/`
-})
-
-// Make an authenticated request to create a post
-const url = `${serverUrl}/ghost/api/v3/admin/themes/upload/`
-const themePath = path.resolve(__dirname, `dist/${themeName}.zip`)
-
-const formData = new FormData()
-// Append any files to the request
-formData.append('file', fs.createReadStream(themePath), { knownLength: fs.statSync(themePath).size })
-const headers = {
-  ...formData.getHeaders(),
-  'Content-Length': formData.getLengthSync(),
-  Authorization: `Ghost ${token}`
-}
-axios
-  .post(url, formData, { headers })
-  .then(response => console.log(JSON.stringify(response.data)))
-  .catch(error => console.error('failed to upload ghost theme', error.response.data))
+deployments.forEach(it => deploy(it))
